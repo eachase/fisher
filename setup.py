21cb2da380a5ee5ab3b432804c6d151683a26cdf
Eve Chase <eve.chase@ligo.org>
2019-12-15 17:34:59 -0600

    Add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="fisher",
+    author="Eve Chase",
+    author_email="evechase2021@u.northwestern.edu",
+    description=("Fisher matrix studies for gravitational-wave parameter estimation."),
+    license="MIT",
+)
