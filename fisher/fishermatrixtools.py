6eff8c353279ddccb7109f4385a7dd75b44a665f
Eve Chase <eve.chase@ligo.org>
2019-12-15 18:49:34 -0600

    fisher/fishermatrixtools.py: include default H1 PSD

--- fisher/fishermatrixtools.py
+++ fisher/fishermatrixtools.py
@@ -1,8 +1,5 @@
-from astropy.cosmology import Planck15, z_at_value
 import numpy as np
-import pycbc.conversions
 import pycbc.psd
-
 
 # Epsilons for finite differencing
 epsilons = {
@@ -12,14 +9,31 @@ epsilons = {
 
 class FisherMatrix(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, event, params,
+        ifo='H1', flow=10, deltaf=0.125, fhigh=2048):
+
+        # Compute waveform, if not already done
+        if not np.all(np.asarray([hasattr(event, attr) for attr \
+            in event._waveattrs+('hx','hp')])):
+            event.waveform(flow=flow, deltaf=deltaf, fhigh=fhigh)
+
+        # Define PSD
+        flen = int(fhigh / deltaf) + 1
+        self.psd = pycbc.psd.from_string('aLIGODesignSensitivityP1200087',
+            flen, deltaf, flow)
+        # FIXME: allow for different PSDs
+
+        # Compute necessary derivatives
+
+        # Form Fisher Matrix
+
 
 
 class FisherParameter(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, parameter):
+        self.epsilon = epsilons[parameter]
+        self.derivative = 0
 
 
 
