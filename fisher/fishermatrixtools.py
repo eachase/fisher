680c2f874510fe4907708339ce7c1b2319575223
Eve Chase <eve.chase@ligo.org>
2019-12-15 18:34:50 -0600

    fisher/fishermatrixtools.py: setup framework

---
+++ fisher/fishermatrixtools.py
@@ -0,0 +1,26 @@
+from astropy.cosmology import Planck15, z_at_value
+import numpy as np
+import pycbc.conversions
+import pycbc.psd
+
+
+# Epsilons for finite differencing
+epsilons = {
+    'mass1': 1e-5,
+    'mass2': 1e-5,
+    }
+
+class FisherMatrix(object):
+
+    def __init__(self):
+        pass
+
+
+class FisherParameter(object):
+
+    def __init__(self):
+        pass
+
+
+
+
